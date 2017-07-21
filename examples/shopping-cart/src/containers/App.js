2a203b4c374d52fad6a05714675bd4bdfd49d0d2
drjkuo <drjkuo@gmail.com>
2017-07-21 10:56:46 -0700

    Add payment page; add router

--- examples/shopping-cart/src/containers/App.js
+++ examples/shopping-cart/src/containers/App.js
@@ -1,17 +1,12 @@
 import React from 'react'
-import ProductsContainer from './ProductsContainer'
-import CartContainer from './CartContainer'
-import NavComponent from '../components/NavComponent'
+import Main from './Main'
 
 const App = () => (
 
   <div className="container">
     {/* <h2>Shopping Cart Example</h2> */}
-    <NavComponent className="row"/>
-    <hr/>
-    <ProductsContainer className="row"/>
-    <hr/>
-    <CartContainer className="row"/>
+    <Main />
+
   </div>
 )
 
---
+++ examples/shopping-cart/src/containers/Home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+// import { Switch, Route } from 'react-router-dom'
+import ProductsContainer from './ProductsContainer'
+import CartContainer from './CartContainer'
+import NavComponent from '../components/NavComponent'
+
+// The Main component renders one of the three provided
+// Routes (provided that one matches). Both the /roster
+// and /schedule routes will match any pathname that starts
+// with /roster or /schedule. The / route will only match
+// when the pathname is exactly the string "/"
+const Main = () => (
+  <div>
+      <NavComponent className="row"/>
+      <hr/>
+      <ProductsContainer className="row"/>
+      <hr/>
+      <CartContainer className="row"/>
+  </div>
+)
+
+export default Main
