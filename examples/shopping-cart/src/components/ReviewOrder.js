ac7e9fb0e10fb43181478114317d56b79ea11657
drjkuo <drjkuo@gmail.com>
2017-07-21 17:08:02 -0700

    Payment page ready

--- examples/shopping-cart/src/components/ReviewOrder.js
+++ examples/shopping-cart/src/components/ReviewOrder.js
@@ -1,20 +1,31 @@
 import React from 'react'
 import axios from 'axios';
 import base64 from 'base-64';
+import Product from './Product';
 
 export default class ReviewOrder extends React.Component {
 
   constructor(props) {
     super(props);
+    // var products = [];
+    this.state = { nodes: [], orderTotal: 0};
 
-    this.state = { cartItems: ''};
-
-    // this.onInputChange=this.onInputChange.bind(this);
+    // const nodes = (
+    //   products.map(product =>
+    //     <Product
+    //       title={product.title}
+    //       price={product.price}
+    //       quantity={product.quantity}
+    //       key={product.id}
+    //     />
+    //   )
+    // );
+    this.componentWillMount=this.componentWillMount.bind(this, this.state.nodes);
     // this.onFormSubmit=this.onFormSubmit.bind(this);
   };
 
-  componentDidMount () {
-    var self = this;
+  componentWillMount (products) {
+    // var self = this;
     var CART_URL = 'http://stage.api.neolife.com/v1/promote/cart.json?';
     var session = '149b6050-e054-45ba-a5f3-bbdce41eff25';
     let tmpString = 'RetailSession:' + session; //0e1b2984-9981-42f8-8e92-db3e2d86401f';
@@ -22,9 +33,29 @@ export default class ReviewOrder extends React.Component {
     const url = CART_URL + 'authorization=' + base64.encode(tmpString);
 
     axios.get(url)
-    .then(function(response) {
+    .then((response) => {
       console.log(response.data);
-      self.setState({ cartItems: response.data});
+      var cartItems = response.data.Items;
+      // self.setState({ cartItems: response.data.Items});
+      // console.log(response.data.Items);
+      console.log('self cart', cartItems);
+      const tmp = cartItems.map (product =>
+        <Product
+          // var tmp = {};
+          // tmp.title = obj.Title;
+          // tmp.price = obj.Price;
+          // tmp.quantity = obj.TodaysQuantity;
+          // tmp.id = obj.SKU;
+          title={product.Title}
+          price={parseInt(product.Price.replace( /^\D+/g, ''), 10)}
+          quantity={parseInt(product.TodaysQuantity, 10)}
+          key={product.SKU}
+          // self.products.push(tmp);
+        />);
+      // // self.state.nodes = cartItems.map (product =>
+      this.setState({nodes: tmp});
+      this.setState({orderTotal: response.data.TotalRetail});
+      console.log('self nodes', this.state.nodes);
       // console.log(response.status);
       // console.log(response.statusText);
       // console.log(response.headers);
@@ -42,10 +73,10 @@ export default class ReviewOrder extends React.Component {
             Review Order <div className="pull-right"><small><a className="afix-1" href="#">Edit Cart</a></small></div>
           </div>
           <div className="panel-body">
-
-            <div className="form-group">
+            <div>{this.state.nodes}</div>
+            {/* <div className="form-group">
               <div className="col-sm-3 col-xs-3">
-                {/* <img className="img-responsive" src="//c1.staticflickr.com/1/466/19681864394_c332ae87df_t.jpg" /> */}
+                <img className="img-responsive" src="//c1.staticflickr.com/1/466/19681864394_c332ae87df_t.jpg" />
               </div>
               <div className="col-sm-6 col-xs-6">
                 <div className="col-xs-12">Product name</div>
@@ -55,11 +86,11 @@ export default class ReviewOrder extends React.Component {
                 <h6><span>$</span>25.00</h6>
               </div>
             </div>
-            {/* <div className="form-group"><hr /></div> */}
+            <div className="form-group"><hr /></div>
 
             <div className="form-group">
               <div className="col-sm-3 col-xs-3">
-                {/* <img className="img-responsive" src="//c1.staticflickr.com/1/466/19681864394_c332ae87df_t.jpg" /> */}
+                <img className="img-responsive" src="//c1.staticflickr.com/1/466/19681864394_c332ae87df_t.jpg" />
               </div>
               <div className="col-sm-6 col-xs-6">
                 <div className="col-xs-12">Product name</div>
@@ -69,11 +100,11 @@ export default class ReviewOrder extends React.Component {
                 <h6><span>$</span>25.00</h6>
               </div>
             </div>
-            {/* <div className="form-group"><hr /></div> */}
+            <div className="form-group"><hr /></div>
 
             <div className="form-group">
               <div className="col-sm-3 col-xs-3">
-                {/* <img className="img-responsive" src="//c1.staticflickr.com/1/466/19681864394_c332ae87df_t.jpg" /> */}
+                <img className="img-responsive" src="//c1.staticflickr.com/1/466/19681864394_c332ae87df_t.jpg" />
               </div>
               <div className="col-sm-6 col-xs-6">
                 <div className="col-xs-12">Product name</div>
@@ -82,10 +113,10 @@ export default class ReviewOrder extends React.Component {
               <div className="col-sm-3 col-xs-3 text-right">
                 <h6><span>$</span>50.00</h6>
               </div>
-            </div>
+            </div> */}
             {/* <div className="form-group"><hr /></div> */}
 
-            <div className="form-group">
+            {/* <div className="form-group">
               <div className="col-xs-12">
                 <strong>Subtotal</strong>
                 <div className="pull-right"><span>$</span><span>200.00</span></div>
@@ -94,17 +125,17 @@ export default class ReviewOrder extends React.Component {
                 <small>Shipping</small>
                 <div className="pull-right"><span>-</span></div>
               </div>
-            </div>
+            </div> */}
 
             {/* <div className="form-group"><hr /></div> */}
 
             <div className="form-group">
               <div className="col-xs-12">
                 <strong>Order Total</strong>
-                <div className="pull-right"><span>$</span><span>150.00</span></div>
+                <div className="pull-right"><span>$</span><span>{this.state.orderTotal}</span></div>
               </div>
             </div>
-            
+
           </div>
         </div>
         {/*REVIEW ORDER END*/}
